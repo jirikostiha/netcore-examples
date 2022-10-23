c54e70294982a34f0bcf96e5f928ef87cd20c5be
Jiri Kostiha <ijkdata@gmail.com>
2022-10-23 22:27:04 +0200

    system examples proj

---
+++ src/System/Program.cs
@@ -0,0 +1,16 @@
+﻿using NetCoreExamples.System.Threading.Tasks;
+using System;
+using System.Threading.Tasks;
+
+namespace NetCoreExamples.System
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            
+
+            Console.ReadKey();
+        }
+    }
+}
