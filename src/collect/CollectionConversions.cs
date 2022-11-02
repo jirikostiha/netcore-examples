a8168ff1c9de9cc68eb77a4b89c0595b871f95bd
Jiri Kostiha <ijkdata@gmail.com>
2022-11-02 18:56:03 +0100

    collections

---
+++ src/collect/CollectionConversions.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace collect
+{
+    [MemoryDiagnoser]
+    public class CollectionConversions
+    {
+        [Params(100, 10_000)]
+        public int N;
+
+        private List<int> _list;
+
+
+        [IterationSetup]
+        public void IterationSetup()
+        {
+            _list = new List<int>(Enumerable.Range(0, N-1));
+        }
+
+        [Benchmark]
+        public int[] ListToArray()
+        {
+            return _list.ToArray();
+        }
+
+        [Benchmark]
+        public List<int> ListToList()
+        {
+            return _list.ToList();
+        }
+    }
+}
